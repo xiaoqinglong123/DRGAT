16e0170cf0eb077cfa273df35bcc781b93521e67
xiaoqinglong123 <40973871+xiaoqinglong123@users.noreply.github.com>
2022-06-10 12:28:09 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,14 +1,12 @@
 from param import parameter_parser
 from DRMGCN import DRMGCN
 from dataprocessing import data_pro
-from utils import show_auc
 import torch
 import numpy as np
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import scipy.io as sio
 import torch.nn.functional as F
-
 
 def lossF(lossType, predictions, targets):
     predictions=predictions.flatten()
@@ -25,19 +23,17 @@ def lossF(lossType, predictions, targets):
             losses = F.binary_cross_entropy_with_logits(predictions.double(), targets.double(), weight=weightTensor)
         else:
             losses = F.binary_cross_entropy(predictions.double(), targets.double(), weight=weightTensor)
-    elif lossType == 'MF_all':
-        losses = torch.pow((predictions - targets), 2).mean()
+
     return losses
 
-def train(model,dataset,A0,ldi, optimizer, opt,i,fold_ROC,fold_AUPR):
+def train(model,dataset,traindata,ldi, optimizer, opt):
 
-   
     model.train()
 
     for epoch in range(0, opt.epoch):
         model.zero_grad()
         score = model(dataset)
-        loss =lossF('cross_entropy', score, A0.cuda())
+        loss =lossF('cross_entropy', score, traindata.cuda())
         loss.backward()
         optimizer.step()
         print(loss.item())
@@ -45,8 +41,6 @@ def train(model,dataset,A0,ldi, optimizer, opt,i,fold_ROC,fold_AUPR):
         scoremin, scoremax = score.min(), score.max()
         score = (score - scoremin) / (scoremax - scoremin)
     return score
-
-
 
 
 def crossCV(args):
@@ -58,19 +52,14 @@ def crossCV(args):
     A = torch.from_numpy(ldi).float()
     A = A.cuda()
 
-    fold_ROC = []
-    fold_AUPR = []
-    
     dataset = data_pro(args)
-    A0 = A.clone()
+    traindata = A.clone()
 
     model = DRMGCN(args)
     model.cuda()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    fold_ROC, fold_AUPR = train(model, dataset,A0,ldi, optimizer, args,i,fold_ROC,fold_AUPR)
-
-
+    train(model, dataset,traindata,ldi, optimizer, args)
 
 if __name__ == "__main__":
     args = parameter_parser()
